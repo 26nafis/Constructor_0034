7b506adc40337df67e117b652c60ce2fa0d2212d
26nafis <spalanzaninafis@gmail.com>
2025-05-17 15:27:00 +0700

    mencetak nim dan nama

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -31,4 +31,8 @@ mahasiswa :: mahasiswa (int iNim, string iNama){
     nim=iNim;
     nama= iNama;
 };
+void mahasiswa::cetak(){
+    cout << endl << "nim =" << nim << endl;
+    cout << " Nama =" <<nama << endl;
+};
 
