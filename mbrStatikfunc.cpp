cf5605101e345fe867ade2f8bab36841f26bc74d
26nafis <spalanzaninafis@gmail.com>
2025-05-17 15:28:52 +0700

    membuat class public dan menampilkna id nama dan mengambil nim dan nama

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -5,5 +5,17 @@ using namespace std;
 class mahasiswa {
 private:
     static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    static void setNim(int pNim){nim=pNim; /*definisi function*/}
+    static int getNim (){return nim; /*definifi function*/}
+
+    mahasiswa (string pnama):nama(pnama){setID();}
+};
 
 
